4837c89858e5afc2e1ba9f45f0547ed10e1c367a
dumayt <dumayt@imsweb.com>
2018-10-01 17:22:11 -0400

    add test

--- ims/encryptedfile/tests/test_encrypt.py
+++ ims/encryptedfile/tests/test_encrypt.py
@@ -1,8 +1,10 @@
 import base
 import os
 from ..interfaces import IEncryptionUtility
+from ..utility import EncryptionUtility
 from zope.component import getUtility
 from plone.namedfile.file import NamedFile
+from plone import api
 
 
 base_path = os.path.dirname(os.path.realpath(__file__))
@@ -10,16 +12,85 @@ base_path = os.path.dirname(os.path.realpath(__file__))
 
 class TestEncrypt(base.IntegrationTestCase):
 
-    def test_encrypt(self):
+    def test_encrypt_7z(self):
         util = getUtility(IEncryptionUtility)
         file_name = u'file_to_encrypt.txt'
         f = open(os.path.join(base_path, file_name))
         file_data = NamedFile(f.read(), filename=file_name)
-        file_format = u'txt'
+        file_format = u'7z'
         password = u'testpass'
-        #3gotta fix this
         blob = util.encrypt(file_data, file_format, file_name, password)
-        self.assertIsInstance(NamedFile, blob)
+        #checking the type
+        self.assertIsInstance(blob, NamedFile)
+
+    def test_decrypt_7z(self):
+        util = getUtility(IEncryptionUtility)
+        file_name = u'file_to_encrypt.txt'
+        f = open(os.path.join(base_path, file_name))
+        file_data = NamedFile(f.read(), filename=file_name)
+        file_format = u'7z'
+        password = u'testpass'
+        blob = util.encrypt(file_data, file_format, file_name, password)
+        d_file_data, d_file_name = util.decrypt(blob, password)
+        #comparing the decrypted file data to what was actually written in the file
+        self.assertEqual(d_file_data, "File to be encrypted.")
+        self.assertEqual(d_file_name, file_name)
+
+    def test_encrypt_zip(self):
+        util = getUtility(IEncryptionUtility)
+        file_name = u'file_to_encrypt.txt'
+        f = open(os.path.join(base_path, file_name))
+        file_data = NamedFile(f.read(), filename=file_name)
+        file_format = u'zip'
+        password = u'testpass'
+        blob = util.encrypt(file_data, file_format, file_name, password)
+        #checking the type
+        self.assertIsInstance(blob, NamedFile)
+
+    def test_decrypt_zip(self):
+        util = getUtility(IEncryptionUtility)
+        file_name = u'file_to_encrypt.txt'
+        f = open(os.path.join(base_path, file_name))
+        file_data = NamedFile(f.read(), filename=file_name)
+        file_format = u'zip'
+        password = u'testpass'
+        blob = util.encrypt(file_data, file_format, file_name, password)
+        d_file_data, d_file_name = util.decrypt(blob, password)
+        #comparing the decrypted file data to what was actually written in the file
+        self.assertEqual(d_file_data, "File to be encrypted.")
+        self.assertEqual(d_file_name, file_name)
+
+    def test_encrypt_folder_multiple(self):
+        util = getUtility(IEncryptionUtility)
+        portal = api.portal.get()
+        file_format = u'zip'
+        password = u'testpass'
+        blob = util.encrypt_folder_multiple(portal, file_format, password)
+        self.assertIsInstance(blob, NamedFile)
+
+
+    def test_encrypt_folder_single(self):
+        import pdb
+        pdb.set_trace()
+        util = getUtility(IEncryptionUtility)
+        portal = api.portal.get()
+        obj1 = api.content.create(
+            type='Document',
+            title='Test document 1',
+            container=portal)
+        obj2 = api.content.create(
+            type='Document',
+            title='Test document 2',
+            container=portal)
+        obj3 = api.content.create(
+            type='Document',
+            title='Test document 3',
+            container=portal)
+        file_format = u'zip'
+        password = u'testpass'
+        blob = util.encrypt_folder_single(portal, file_format, password)
+        self.assertIsInstance(blob, NamedFile)
+
 
 
 
