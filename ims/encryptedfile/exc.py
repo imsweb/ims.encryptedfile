f06131bc2d37d0cbcd75a6466e56bf9e55125a69
dumayt <dumayt@imsweb.com>
2018-10-03 14:50:53 -0400

    update testing/error testing

--- ims/encryptedfile/exc.py
+++
@@ -1,3 +0,0 @@
-class DecryptionError(Exception):
-    """ Decryption Error """
-    pass
--- ims/encryptedfile/tests/test_encrypt_decrypt.py
+++ ims/encryptedfile/tests/test_encrypt_decrypt.py
@@ -6,7 +6,7 @@ from plone.namedfile.file import NamedFile
 from plone import api
 from plone.namedfile import NamedFile, NamedImage
 from plone.dexterity.utils import addContentToContainer
-from ims.encryptedfile.exc import DecryptionError
+from ims.encryptedfile.utility import DecryptionError
 
 
 base_path = os.path.dirname(os.path.realpath(__file__))
@@ -14,48 +14,39 @@ base_path = os.path.dirname(os.path.realpath(__file__))
 
 class TestEncrypt(base.IntegrationTestCase):
 
-    def test_encrypt_7z(self):
+    def test_encrypt(self):
         util = getUtility(IEncryptionUtility)
         file_name = u'file_to_encrypt.txt'
         f = open(os.path.join(base_path, file_name))
         file_data = NamedFile(f.read(), filename=file_name)
-        file_format = u'7z'
         password = u'testpass'
+        ##7z
+        file_format = u'7z'
         blob = util.encrypt(file_data, file_format, file_name, password)
         #checking the type
         self.assertIsInstance(blob, NamedFile)
+        ##zip
+        file_format = u'zip'
+        blob = util.encrypt(file_data, file_format, file_name, password)
+        # checking the type
+        self.assertIsInstance(blob, NamedFile)
 
-    def test_decrypt_7z(self):
+    def test_decrypt(self):
         util = getUtility(IEncryptionUtility)
         file_name = u'file_to_encrypt.txt'
         f = open(os.path.join(base_path, file_name))
         file_data = NamedFile(f.read(), filename=file_name)
-        file_format = u'7z'
         password = u'testpass'
+        ##7z
+        file_format = u'7z'
         blob = util.encrypt(file_data, file_format, file_name, password)
         d_file_data, d_file_name = util.decrypt(blob, password)
         #comparing the decrypted file data to what was actually written in the file
         self.assertEqual(d_file_data, "File to be encrypted.")
         self.assertEqual(d_file_name, file_name)
 
-    def test_encrypt_zip(self):
-        util = getUtility(IEncryptionUtility)
-        file_name = u'file_to_encrypt.txt'
-        f = open(os.path.join(base_path, file_name))
-        file_data = NamedFile(f.read(), filename=file_name)
-        file_format = u'7z'
-        password = u'testpass'
-        blob = util.encrypt(file_data, file_format, file_name, password)
-        #checking the type
-        self.assertIsInstance(blob, NamedFile)
-
-    def test_decrypt_zip(self):
-        util = getUtility(IEncryptionUtility)
-        file_name = u'file_to_encrypt.txt'
-        f = open(os.path.join(base_path, file_name))
-        file_data = NamedFile(f.read(), filename=file_name)
+        ##zip
         file_format = u'zip'
-        password = u'testpass'
         blob = util.encrypt(file_data, file_format, file_name, password)
         d_file_data, d_file_name = util.decrypt(blob, password)
         #comparing the decrypted file data to what was actually written in the file
@@ -83,12 +74,20 @@ class TestEncrypt(base.IntegrationTestCase):
             title='Test document 3',
             container=portal)
         obj3.file = NamedFile("The mars rover.!2", filename=u'testfile3.txt')
-
-        file_format = u'7z'
         password = u'testpass'
+
+        #7z
+        file_format = u'7z'
         blob = util.encrypt_folder_multiple(portal, file_format, password)
         self.assertIsInstance(blob, NamedFile)
         #error testing
+        self.assertRaises(DecryptionError, util.decrypt(blob, password))
+
+        # zip
+        file_format = u'zip'
+        blob = util.encrypt_folder_multiple(portal, file_format, password)
+        self.assertIsInstance(blob, NamedFile)
+        # error testing
         self.assertRaises(DecryptionError, util.decrypt(blob, password))
 
 
@@ -99,20 +98,28 @@ class TestEncrypt(base.IntegrationTestCase):
             type='File',
             title='Test document 1',
             container=portal)
-
         obj1.file = NamedFile("Text that needs encryption", filename=u'testfile.txt')
-        file_format = u'zip'
         password = u'testpass'
+
+        #7z
+        file_format = u'7z'
         blob = util.encrypt_folder_single(portal, file_format, password)
         self.assertIsInstance(blob, NamedFile)
-        import pdb
-        pdb.set_trace()
         d_file_data, d_file_name = util.decrypt(blob, password)
         # making sure it returns a zip file
         num = len(d_file_name) - 4 #for .zip
         zip = d_file_name[num:]
         self.assertEqual(zip, '.zip')
 
+        #zip
+        file_format = u'zip'
+        blob = util.encrypt_folder_single(portal, file_format, password)
+        self.assertIsInstance(blob, NamedFile)
+        d_file_data, d_file_name = util.decrypt(blob, password)
+        # making sure it returns a zip file
+        num = len(d_file_name) - 4 #for .zip
+        zip = d_file_name[num:]
+        self.assertEqual(zip, '.zip')
 
 def test_suite():
     import unittest
